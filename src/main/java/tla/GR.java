1e839827aeb2b2de25505e6f3d6fadaa914209db
lsoncini <lsoncini@itba.edu.ar>
2017-02-09 23:02:41 -0300

    Validator, Simplifier and Squasher for Regular Grammars

--- src/main/java/tla/GR.java
+++ src/main/java/tla/GR.java
@@ -1,9 +1,11 @@
 package tla;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class GR {
 
@@ -11,15 +13,21 @@ public class GR {
 	protected Set<String> terminals;
 	protected Map<String, Set<String>> predicates;
 	protected String initialState;
+	
+	private static final String LAMBDA = "\\";
 
 	public GR(final Set<String> nt, final Set<String> t, final Map<String, Set<String>> p, final String is) throws IllegalArgumentException{
-		if(!isValid())
-			throw new IllegalArgumentException();
 		
+		if(!Collections.disjoint(nt, t))
+				throw new IllegalArgumentException("Non Terminals and Terminals must be disjoint");
 		this.setNonTerminals(nt)
 			.setTerminals(t)
-			.setPredicates(p)
-			.setInitialState(is);
+			.setPredicates(p);
+		if(!setInitialState(is))
+			throw new IllegalArgumentException("Initial symbol not included in Non-Terminal set.");
+		if(!isValid())
+			throw new IllegalArgumentException("Not a regular grammar.");
+		this.simplify();
 	}
 
 	/*
@@ -67,6 +75,16 @@ public class GR {
 		this.predicates.put(from, p);
 		return Boolean.TRUE;
 	}
+	
+	public boolean removePredicate(String from, String to) {
+		Set<String> p = predicates.get(from);
+		if(!p.contains(to))
+			return Boolean.FALSE;
+		p.remove(to);
+		this.predicates.put(from, p);
+		return Boolean.TRUE;
+	}
+	
 	public boolean addPredicates(Map<String,Set<String>> predicates) {
 		boolean flag = true;
 		for(Entry<String, Set<String>> p : predicates.entrySet()) {
@@ -80,16 +98,18 @@ public class GR {
 		return initialState;
 	}
 
-	public GR setInitialState(String initialState) {
+	public boolean setInitialState(String initialState) {
+		if (!nonTerminals.contains(initialState))
+			return Boolean.FALSE;
 		this.initialState = initialState;
-		return this;
+		return Boolean.TRUE;
 	}
 
 	public GR toRight() {
 		boolean isGRD = true;
 		Map<String, Set<String>> ps = new HashMap<String, Set<String>>();
 		Set<String> lambda = new HashSet<>();
-		lambda.add("\\");
+		lambda.add(LAMBDA);
 		String initial = getNextState();
 		ps.put(initialState, lambda);
 		for (Entry<String, Set<String>> p : predicates.entrySet()) {
@@ -103,7 +123,7 @@ public class GR {
 				} else if (strings.length == 1) {
 					Set<String> newStateList = ps.getOrDefault(initial, new HashSet<>());
 					
-					if (strings[0].equals("\\")) {
+					if (strings[0].equals(LAMBDA)) {
 						newStateList.add(s);
 						ps.put(initial, newStateList);
 					} else if (nonTerminals.contains(strings[0])) {
@@ -130,15 +150,112 @@ public class GR {
 	}
 
 	private boolean isValid() {
-		//TODO check regular
-		return true; //TODO do something
+		for(Entry<String, Set<String>> ps : predicates.entrySet()) {
+			if(!nonTerminals.contains(ps.getKey()))
+				return false;
+			for(String p : ps.getValue()) {
+				if (p.equals(LAMBDA))
+					continue;
+				String[] symbols = p.split(" ");
+				boolean hasNT = false;
+				boolean hasT = false;
+				for (int i = 0; i < symbols.length; i++) {
+					if(nonTerminals.contains(symbols[i])) {
+						if(hasNT)
+							return false;
+						hasNT = true;
+					} else if(terminals.contains(symbols[i])) {
+						if(hasT)
+							return false;
+						hasT = true;
+					}
+				}
+				if(!hasNT && !hasT)
+					return false;
+			}
+		}
+		for (String state : nonTerminals) {
+			if(!predicates.containsKey(state) || predicates.get(state).isEmpty())
+				return false;
+		}
+		return true;
 	}
 	
 	private GR squash() {
-		return this; //TODO do something
+		for(String nt : nonTerminals) {
+			boolean hasChanged = false;
+			do {
+				hasChanged = false;
+				Set<String> ps = predicates.get(nt);
+				for(String p : ps) {
+					String[] symbols = p.split(" ");
+					if(symbols.length == 1 && nonTerminals.contains(p)){
+						this.removePredicate(nt, p);
+						this.addPredicate(nt, predicates.get(p));
+						hasChanged = true;
+					}
+				}
+			} while (hasChanged);
+			
+		}
+		return this.simplify();
 	}
 	
 	private String getNextState() {
 		return "FIDEL";
+	}
+	
+	private GR simplify() {
+		Set<String> productiveSymbols = new HashSet<>();
+		Set<String> reachableSymbols = new HashSet<>();
+		reachableSymbols.add(initialState);
+		for(Entry<String, Set<String>> ps : predicates.entrySet()) {
+			for(String p : ps.getValue()) {
+				if(terminals.contains(p) || p.equals(LAMBDA)) {
+					productiveSymbols.add(ps.getKey());
+					break;
+				}
+			}
+		}
+		boolean hasChanged = false;
+		do {
+			hasChanged = false;
+			for(Entry<String, Set<String>> ps : predicates.entrySet()) {
+				String key = ps.getKey();
+				if(productiveSymbols.contains(key) && !reachableSymbols.contains(key))
+					continue;
+				for(String p : ps.getValue()) {
+					String[] s = p.split(" ");
+					String nt = null;
+					for (int i = 0; i < s.length; i++) {
+						if(nonTerminals.contains(s[i])) {
+							nt = s[i];
+							break;
+						}
+					}
+					
+					if(nt != null){
+						if(!productiveSymbols.contains(key) && productiveSymbols.contains(nt)) {
+							productiveSymbols.add(key);
+							hasChanged = true;
+							if(!reachableSymbols.contains(key))
+								break;
+						}
+						if(reachableSymbols.contains(key) && !reachableSymbols.contains(nt)) {
+							reachableSymbols.add(nt);
+							hasChanged = true;
+						}
+					}
+				}
+			}
+		} while (hasChanged);
+		
+		Set<String> killSet = nonTerminals.stream().filter(nt -> !productiveSymbols.contains(nt) || !reachableSymbols.contains(nt)).collect(Collectors.toSet());
+		killSet.forEach(nt -> remove(nt));
+		return this;
+	}
+	
+	private void remove(String nt){
+		//TODO remove everything
 	}
 }
